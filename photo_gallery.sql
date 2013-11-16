08dd254fcc3f0e0991c7aeb799f18db7a6695102
Christian Landin <christian.landin@me.com>
2013-11-16 13:04:19 +0100

    db dump

---
+++ photo_gallery.sql
@@ -0,0 +1,44 @@
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL auto_increment,
+  `photograph_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `photograph_id` (`photograph_id`)
+) AUTO_INCREMENT=1;
+
+--
+-- Table structure for table `photographs`
+--
+
+DROP TABLE IF EXISTS `photographs`;
+CREATE TABLE `photographs` (
+  `id` int(11) NOT NULL auto_increment,
+  `filename` varchar(255) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `size` int(11) NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) AUTO_INCREMENT=1;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  PRIMARY KEY  (`id`)
+) AUTO_INCREMENT=1;
+
